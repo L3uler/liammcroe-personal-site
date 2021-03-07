807448ca602f6806dce67139a81d2f150a15082e
Liam McRoe <liammcroe1995@gmail.com>
2021-03-07 16:21:39 +0000

    github and linkedin logos and some resizing

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,27 @@
 // components/Header.js
 
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faGithubSquare,
+    faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
+
 import styles from "./Header.module.scss";
 
 const Header = props => (
   <div className={styles.Header}>
     <div className={styles.Typewriter}>
-      {props.appTitle}
+      <Link href="/">
+        {props.appTitle}
+      </Link>
     </div>
+    <div className={styles.Buttons}>
+      <a href="https://github.com/L3uler"><FontAwesomeIcon icon={faGithubSquare}/></a>
+      
+      <a href="https://www.linkedin.com/in/liammcroe/"><FontAwesomeIcon icon={faLinkedin}/></a>
+    </div>
+    
   </div>
 );
 
