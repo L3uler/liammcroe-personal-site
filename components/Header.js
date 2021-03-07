39a6cb0094b6cec31473f6036e4a4260da17a74f
Liam McRoe <liammcroe1995@gmail.com>
2021-03-07 16:24:12 +0000

    format fixing

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,8 @@
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faGithubSquare,
-    faLinkedin
+  faGithubSquare,
+  faLinkedin
 } from "@fortawesome/free-brands-svg-icons";
 
 import styles from "./Header.module.scss";
@@ -12,16 +12,14 @@ import styles from "./Header.module.scss";
 const Header = props => (
   <div className={styles.Header}>
     <div className={styles.Typewriter}>
-      <Link href="/">
-        {props.appTitle}
-      </Link>
+      {props.appTitle}
     </div>
     <div className={styles.Buttons}>
-      <a href="https://github.com/L3uler"><FontAwesomeIcon icon={faGithubSquare}/></a>
-      
-      <a href="https://www.linkedin.com/in/liammcroe/"><FontAwesomeIcon icon={faLinkedin}/></a>
+      <a href="https://github.com/L3uler"><FontAwesomeIcon icon={faGithubSquare} /></a>
+
+      <a href="https://www.linkedin.com/in/liammcroe/"><FontAwesomeIcon icon={faLinkedin} /></a>
     </div>
-    
+
   </div>
 );
 
