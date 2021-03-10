52ba73887ba7af1d4d7c8bad6cf66e53226e8b29
Liam McRoe <liammcroe1995@gmail.com>
2021-03-10 13:24:50 +0000

    fixing alignment for hompage image and text

--- components/PageSpecific/Index/ProfileImage.js
+++ components/PageSpecific/Index/ProfileImage.js
@@ -3,7 +3,12 @@
 import styles from "./ProfileImage.module.scss";
 
 const ProfileImage = (props) => (
+
     <div className={styles.ProfileImage}>
+        <div className={styles.ProfileTitle}>
+            <h1>Hello,</h1>
+            <h2>I'm Liam McRoe, a software developer based in Edinburgh, Scotland.</h2>
+        </div>
         <img src={props.imageSource} />
     </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,7 @@ import ProfileImage from "../components/PageSpecific/Index/ProfileImage";
 
 const Index = () =>
     <Layout>
-        <h1>Liam McRoe</h1>
-
         <ProfileImage imageSource={`/images/poppy.jpg`}/>
-        <h2>Software developer based in Edinburgh, Scotland.</h2>
-
     </Layout>;
 
 export default Index;
