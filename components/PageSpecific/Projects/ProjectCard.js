6b1478e6390d028ee9085c44b7c18ba73b4a2045
Liam McRoe <liammcroe1995@gmail.com>
2021-03-17 22:53:57 +0000

    improving card layout and content on projects page

--- components/PageSpecific/Projects/ProjectCard.js
+++ components/PageSpecific/Projects/ProjectCard.js
@@ -1,4 +1,5 @@
 // components/PageSpecific/Projects/ProjectCard.js
+import { Button } from "reactstrap";
 
 import CardLink from "./CardLink";
 
@@ -7,32 +8,31 @@ import styles from "./ProjectCard.module.scss";
 const ProjectCard = (props) => {
     return (
         <div className={styles.ProjectCard}>
+
             <img src={props.image} />
+
             <div className={styles.container}>
-                
+
+                <div className={styles.CardLinks}>
+                    {props.cardLinks.map((cardLink, index) => (
+
+                        <a key={index} href={cardLink.link} title={cardLink.mouseover} target="_blank">
+                            <Button className={styles.LinkButton}>{cardLink.label}</Button>
+                        </a>
+                    ))}
+                </div>
                 <div className={styles.CardHeader}>
                     <div className={styles.CardTitle}>
                         <h3>{props.title}</h3>
-                    </div>         
+                    </div>
                 </div>
 
                 <div className={styles.CardContent}>
                     {props.content}
                 </div>
 
-                <div className={styles.CardLinks}>
-                        {props.cardLinks.map((cardLink, index) => (
-                            <CardLink
-                                key={index}
-                                link={cardLink.link}
-                                title={cardLink.mouseover}
-                                icon={cardLink.icon}
-                                label={cardLink.label}
-                            />
-                        ))}
-                </div>
-                
             </div>
+
         </div>
     );
 };
--- config/projectCards.js
+++ config/projectCards.js
@@ -1,36 +1,25 @@
 // config/projectCards.js
 
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faGithubSquare
-  } from "@fortawesome/free-brands-svg-icons";
-import {
-    faLink
-} from "@fortawesome/free-solid-svg-icons";
-
 const projectCards = [
     {
         title: "BottleService",
-        content: "A lightweight .NET 5 microservice I wrote for an alcohol collection management app. The service follows the ports and adapter pattern, exposing an ASP.NET Core REST API and using MongoDB for storage. It allows clients to perform CRUD operations on Bottle resources, the central domain entity for the app. The service has been containerised with Docker.",
+        content: "This is a lightweight .NET 5 microservice I wrote for a Whisky and Wine cellar management app. The service follows the ports and adapter pattern, exposing an ASP.NET Core REST API and using MongoDB for storage. It also uses a bunch of popular .NET libraries to do validation, mapping, and a bunch of other stuff. It allows clients to perform CRUD operations on Bottle resources, the central domain entity for the application. The service has been containerised with Docker for ease of development and deployment. I've uploaded it to my GitHub as a bit of a sample, to showcase a backend service that I architected and built using a range of modern technologies. Feel free to dig into the code.",
         image: "/images/BottleService.jpg",
         cardLinks: [
             {
                 link: "https://github.com/L3uler/bottle-service",
-                icon: <FontAwesomeIcon icon={faGithubSquare} />,
                 mouseover: "View code on GitHub",
-                label: "View code"
+                label: "View code "
             }
         ]
     },
     {
         title: "This Site",
-        content: "This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content.",
+        content: "I decided to put this site together to get some more experience of front-end development using modern frameworks. I built it from scratch using the Next.js framework and React. It's hosted via Vercel, which provides a CI/ CD pipeline that integrates directly with GitHub for a seemless, modern deployment strategy. Speaking of GitHub, the entire codebase can be found in a public repo on my page, so check it out!",
         image: "/images/PersonalSite.jpg",
         cardLinks: [
             {
                 link: "https://github.com/L3uler/liammcroe-personal-site",
-                icon: <FontAwesomeIcon icon={faGithubSquare} />,
                 mouseover: "View code on GitHub",
                 label: "View code"
             }
@@ -41,10 +30,7 @@ const projectCards = [
         content: "A lightweight .NET 5 microservice I wrote for an alcohol collection management app. The service follows the ports and adapter pattern, exposing an ASP.NET Core REST API and using MongoDB for storage. It allows clients to perform CRUD operations on Bottle resources, the central domain entity for the app. The service has been containerised with Docker.",
         image: "/images/BottleService.jpg",
         cardLinks: [
-            {
-                link: "https://github.com/L3uler/bottle-service",
-                icon: <FontAwesomeIcon icon={faGithubSquare} /> 
-            }
+            
         ]
     },
     {
@@ -52,10 +38,7 @@ const projectCards = [
         content: "This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content.",
         image: "/images/PersonalSite.jpg",
         cardLinks: [
-            {
-                link: "https://github.com/L3uler/liammcroe-personal-site",
-                icon: <FontAwesomeIcon icon={faGithubSquare} /> 
-            }
+           
         ]
     }
 ];
